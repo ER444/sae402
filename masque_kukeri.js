b2ab65d1f6214a5f0cc388768291839ced509de6
Feryel Khelifi <feryelkhelifi@gmail.com>
2024-04-01 17:10:43 +0200

    ouioui

--- masque_kukeri.js
+++ masque_kukeri.js
@@ -1,23 +1,25 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 // Créer une scène Three.js
+let obj;
 const scene = new THREE.Scene();
 const light = new THREE.AmbientLight(0xffffff); // lumière blanche
-        scene.add(light);
+scene.add(light);
 
-// Créer un chargeur GLTFLoader
-const loader = new GLTFLoader();
 
-// Charger l'objet glTF
-loader.load(
-    '3D/mask.glb',
-    function (gltf) {
-        // Ajouter l'objet chargé à la scène
-        scene.add(gltf.scene);
+const mtlLoader = new MTLLoader();
+mtlLoader.load('3D/mask.mtl', function (materials) {
+    materials.preload();
+
+    const loader = new OBJLoader();
+    loader.setMaterials(materials);
+
+    loader.load('3D/mask.obj', function (object) {
+        obj = object;
+        scene.add(obj);
     },
-    // Progression du chargement
     function (xhr) {
         console.log((xhr.loaded / xhr.total * 100) + '% chargé');
     },
@@ -25,23 +27,28 @@ loader.load(
     function (error) {
         console.error( error);
     }
-);
+    )
+})
 
 // Créer un rendu
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const threejsContainer = document.getElementById('threejs-container');
+threejsContainer.appendChild(renderer.domElement);
 
 
 // Créer une caméra
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+camera.position.y = 2 ;
 
 // Animation de rendu
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    obj.rotation.y += 0.01;
 }
+
 
 animate();
 
